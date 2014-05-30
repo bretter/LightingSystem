c474bf97e65ea961089c3efb2ad358a6ffe57769
Brett Nelson <brettnelson@ufl.edu>
2014-05-30 15:34:47 -0400

    fixed file path

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 ## Startup script to launch all python services
 
-sudo python3 ~/VisualAlert/VisualAlertForPi.py
-sudo python3 ~/TempSensor/ReadTemp.py
-sudo python  ~/webServer/StartHTTPServer.py
+sudo python3 ~/LightingSystem/VisualAlert/VisualAlertForPi.py &
+sudo python3 ~/LightingSystem/TempSensor/ReadTemp.py &
+sudo python  ~/LightingSystem/webServer/StartHTTPServer.py &
