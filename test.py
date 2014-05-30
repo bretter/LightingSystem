94da7b372db0265b807a8bf50c8f22e847cf78de
jdryden572 <jdryden572@gmail.com>
2014-05-30 15:54:05 -0400

    deleted old VisualAlert.py

--- test.py
+++ test.py
@@ -1,46 +1,6 @@
-### This is a test script used when writing VisualAlert.py ###
+import urllib.request
 
-redPin = 'red'
-yellowPin = 'yellow'
-greenPin = 'green'
+while True:
 
-class Light:
-	def __init__(self, pin):
-		self.pin = pin
-
-	def On(self):
-		print(self.pin + ' pin ON')
-
-	def Off(self):
-		print(self.pin + ' pin OFF')
-
-class Tower:
-	def __init__(self, redLight, yellowLight, greenLight):
-		self.redLight = redLight
-		self.yellowLight = yellowLight
-		self.greenLight = greenLight
-
-	def Red(self):
-		self.redLight.On()
-		self.yellowLight.Off()
-		self.greenLight.Off()
-
-	def YellowRed(self):
-		self.redLight.On()
-		self.yellowLight.On()
-		self.greenLight.Off()
-
-	def Yellow(self):
-		self.redLight.Off()
-		self.yellowLight.On()
-		self.greenLight.Off()
-
-	def GreenYellow(self):
-		self.redLight.Off()
-		self.yellowLight.On()
-		self.greenLight.On()
-
-	def Green(self):
-		self.redLight.Off()
-		self.yellowLight.Off()
-		self.greenLight.On()
+	print(str(urllib.request.urlopen(
+		'http://jamesrpi.local:8000/currentLightState.txt').read()))
