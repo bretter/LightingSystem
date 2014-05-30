3332972b4e5173bf707468716715159a8ae6ba8e
jdryden572 <jdryden572@gmail.com>
2014-05-30 15:16:53 -0400

    write to .txt

--- VisualAlert/VisualAlertForPi.py
+++ VisualAlert/VisualAlertForPi.py
@@ -95,7 +95,7 @@ def determineState(points, connectionFailure):
 
 def fileWrite(state):
 	text = str(time.time()) + ' ' + str(state)
-	file = open('../webServer/currentLightState.log', 'w')
+	file = open('../webServer/currentLightState.txt', 'w')
 	file.write(text)
 	file.close()
 
