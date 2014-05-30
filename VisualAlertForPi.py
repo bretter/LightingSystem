fde7870ee26b6136c12b49a75ee438c14aee9ddb
Brett Nelson <brettnelson@ufl.edu>
2014-05-30 09:52:09 -0400

    put functions in order of execution

--- VisualAlertForPi.py
+++ VisualAlertForPi.py
@@ -8,6 +8,50 @@ delayTime = 1
 maxDisconnectTime = 30
 lightTower = LightTower.Tower()
 
+def MainLoop():
+	connectFailCount = 0		# create error counter
+	points = 0
+
+	while True:
+		thisTime = time.time()						# record time when entering loop
+		[newCallsWaiting, newTimeSeconds, connectFail] = getData(pageURL)
+		if connectFail:
+			connectFailCount += 1
+		else:
+			callsWaiting = newCallsWaiting
+			timeSeconds = newTimeSeconds
+			points = calcPoints(int(callsWaiting), timeSeconds)
+			connectFailCount = 0
+
+		connectionFailure = connectFailCount*delayTime >= maxDisconnectTime
+		updateDisplay(points, connectionFailure)
+		elapsedTime = time.time() - thisTime		# check time elapsed fetching data
+		if elapsedTime > delayTime:					# proceed if fetching took longer than 5 sec
+			pass
+		else:										# otherwise delay the remainder of 5 sec
+			time.sleep(delayTime - elapsedTime)
+
+def getData(address):
+	try:
+		data = str(urllib.request.urlopen(address).read())	# fetch CISCO phone data
+		connectFail = 0					# reset error counter
+		extracted = searchPattern.search(data)	# extract desired values from data
+		[callsWaiting, minutesWaiting, secondsWaiting] = [
+			extracted.group(1), extracted.group(2), extracted.group(3)]
+		print('{0:2s} calls waiting for {1:s}:{2:2s}'.format(callsWaiting, minutesWaiting, secondsWaiting))
+		timeSeconds = int(secondsWaiting) + int(minutesWaiting)*60
+		return [callsWaiting, timeSeconds, connectFail]
+	except urllib.error.URLError:				# print error if network lost
+		print('CANNOT CONNECT TO CISCO PHONE STATUS PAGE')
+		connectFail = 1					# step fail counter up by 1
+		return [None, None, connectFail]
+
+def calcPoints(calls, waitTime):
+	callPoints = calls
+	timePoints = waitTime // 60
+	points = callPoints + timePoints
+	return points
+
 def updateDisplay(points, connectionFailure):
 
 	# Lis of States
@@ -37,53 +81,6 @@ def updateDisplay(points, connectionFailure):
 		lightTower.setState(redYellow)
 	elif points >= 9:
 		lightTower.setState(red)
-
-def calcPoints(calls, waitTime):
-	callPoints = calls
-	timePoints = waitTime // 60
-	points = callPoints + timePoints
-	return points
-
-def getData(address):
-	try:
-		data = str(urllib.request.urlopen(address).read())	# fetch CISCO phone data
-		connectFail = 0					# reset error counter
-		extracted = searchPattern.search(data)	# extract desired values from data
-		[callsWaiting, minutesWaiting, secondsWaiting] = [
-			extracted.group(1), extracted.group(2), extracted.group(3)]
-		print('{0:2s} calls waiting for {1:s}:{2:2s}'.format(callsWaiting, minutesWaiting, secondsWaiting))
-		timeSeconds = int(secondsWaiting) + int(minutesWaiting)*60
-		return [callsWaiting, timeSeconds, connectFail]
-	except urllib.error.URLError:				# print error if network lost
-		print('CANNOT CONNECT TO CISCO PHONE STATUS PAGE')
-		connectFail = 1					# step fail counter up by 1
-		return [None, None, connectFail]
-
-
-def MainLoop():
-	connectFailCount = 0		# create error counter
-	points = 0
-
-	while True:
-		thisTime = time.time()						# record time when entering loop
-		[newCallsWaiting, newTimeSeconds, connectFail] = getData(pageURL)
-		if connectFail:
-			connectFailCount += 1
-		else:
-			callsWaiting = newCallsWaiting
-			timeSeconds = newTimeSeconds
-			points = calcPoints(int(callsWaiting), timeSeconds)
-			connectFailCount = 0
-
-		connectionFailure = connectFailCount*delayTime >= maxDisconnectTime
-		updateDisplay(points, connectionFailure)
-		elapsedTime = time.time() - thisTime		# check time elapsed fetching data
-		if elapsedTime > delayTime:					# proceed if fetching took longer than 5 sec
-			pass
-		else:										# otherwise delay the remainder of 5 sec
-			time.sleep(delayTime - elapsedTime)
-
-
 
 def resetLights():
 	lightTower.setState([0,0,0])
