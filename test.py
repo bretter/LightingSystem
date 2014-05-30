5112b6ff984f6e43a732cba965505214ba1dc093
Brett Nelson <brettnelson@ufl.edu>
2014-05-30 09:00:45 -0400

    testing merges with test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#this is a comment added by brett
+
 redPin = 'red'
 yellowPin = 'yellow'
 greenPin = 'green'
@@ -5,39 +7,43 @@ greenPin = 'green'
 class Light:
 	def __init__(self, pin):
 		self.pin = pin
-		
+
 	def On(self):
 		print(self.pin + ' pin ON')
-	
+
 	def Off(self):
 		print(self.pin + ' pin OFF')
-		
+
 class Tower:
 	def __init__(self, redLight, yellowLight, greenLight):
 		self.redLight = redLight
 		self.yellowLight = yellowLight
 		self.greenLight = greenLight
-	
+
+
+
+
+
 	def Red(self):
 		self.redLight.On()
 		self.yellowLight.Off()
 		self.greenLight.Off()
-		
+
 	def YellowRed(self):
 		self.redLight.On()
 		self.yellowLight.On()
 		self.greenLight.Off()
-		
+
 	def Yellow(self):
 		self.redLight.Off()
 		self.yellowLight.On()
 		self.greenLight.Off()
-	
+
 	def GreenYellow(self):
 		self.redLight.Off()
 		self.yellowLight.On()
 		self.greenLight.On()
-	
+
 	def Green(self):
 		self.redLight.Off()
 		self.yellowLight.Off()
