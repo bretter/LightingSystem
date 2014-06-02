3e7d6afc3636ddc2e3cfaddc575ad4867ae7825c
Brett Nelson <brettnelson@ufl.edu>
2014-06-02 16:26:15 -0400

    git account not necessary for install

--- install.sh
+++ install.sh
@@ -6,19 +6,15 @@ rm -rf /opt/LightingSystem
 rm /bin/visualalert
 
 ## Clone git repo
-
-git clone -b Dev https://github.com/bretter/LightingSystem.git /opt/LightingSystem
+git clone -b Dev git://github.com/bretter/LightingSystem.git /opt/LightingSystem
 
 ## Make startup script executable
-
 chmod +x /opt/LightingSystem/visualalert.sh
 
 ## Copy over launch script
-
 ln -s /opt/LightingSystem/visualalert.sh /bin/visualalert
 
-## Add to chrontab startup
-
+## Add to appropriate hook to chrontab
 #crontab -l | { cat; echo "@reboot visualalert"; } | crontab -
 crontab -l > crontab.tmp
 echo "@reboot visualalert" >> crontab.tmp
